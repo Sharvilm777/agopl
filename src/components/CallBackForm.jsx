3d872da0da837fb708c2cf6e1f6687a28b8e1cb0
Sharvil <sharvilm777@gmail.com>
2023-07-08 02:03:03 +0530

    Toastify added and Some validation added

--- src/components/CallBackForm.jsx
+++ src/components/CallBackForm.jsx
@@ -1,5 +1,7 @@
 "use client";
 import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const CallBackForm = () => {
   const [contactFormData, setcontactFormData] = useState({
@@ -16,7 +18,9 @@ const CallBackForm = () => {
     }));
   };
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    toast.info("Sending request..");
     const reqOptions = {
       method: "POST",
       headers: {
@@ -28,65 +32,95 @@ const CallBackForm = () => {
       const response = await fetch("/api/sendEmail", reqOptions);
       const resJson = await response.json();
       console.log("Response from server", resJson);
+      toast.success("Request Submitted successfully");
+      toast.info("You will get the call soon...");
+      setcontactFormData({
+        name: "",
+        email: "",
+        phoneNumber: "",
+      });
     } catch (error) {
       console.log("Some errored Occured", error);
+      toast.error("Sorry some error occurred ");
+      toast.info("Message not sent!!! ");
+      setcontactFormData({
+        name: "",
+        email: "",
+        phoneNumber: "",
+      });
     }
   };
 
   return (
-    <div className="contactForm w-full bg-green-900 h-auto ">
-      <div className="contact_heading text-2xl text-center mt-4 font-semibold text-white p-4">
-        Request a Callback
-      </div>
-      <div>
-        <form
-          action=""
-          onSubmit={handleSubmit}
-          className="form flex sm:flex-row flex-col justify-evenly items-center "
-        >
-          <input
-            type="text"
-            name="name"
-            id="name"
-            minLength={2}
-            value={contactFormData.name}
-            onChange={handleOnChange}
-            required
-            placeholder="Name"
-            className="p-4 m-4 bg-slate-800 rounded-md text-white"
-          />
-          <input
-            type="email"
-            name="email"
-            id="email"
-            value={contactFormData.email}
-            onChange={handleOnChange}
-            placeholder="Email"
-            title="Please enter a valid email address."
-            required
-            className="p-4 m-4 bg-slate-800 rounded-md text-white"
-          />
-          <input
-            type="number"
-            name="phoneNumber"
-            id="phoneNumber"
-            maxLength={10}
-            required
-            title="Please enter a valid Phone number"
-            value={contactFormData.phoneNumber}
-            onChange={handleOnChange}
-            placeholder="Phone Number"
-            className="p-4 m-4 bg-slate-800 rounded-md text-white"
-          />
-          <button
-            type="submit"
-            className="bg-green-700 mb-4 pl-24 pr-24 pt-5 pb-5 rounded-md"
+    <>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <div className="contactForm w-full bg-green-900 h-auto ">
+        <div className="contact_heading text-2xl text-center mt-4 font-semibold text-white p-4">
+          Request a Callback
+        </div>
+        <div>
+          <form
+            method="POST"
+            onSubmit={handleSubmit}
+            className="form flex sm:flex-row flex-col justify-evenly items-center "
           >
-            Send Now
-          </button>
-        </form>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              minLength={2}
+              value={contactFormData.name}
+              onChange={handleOnChange}
+              required
+              placeholder="Name"
+              className="p-4 m-4 bg-slate-800 rounded-md text-white"
+            />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={contactFormData.email}
+              onChange={handleOnChange}
+              placeholder="Email"
+              title="Please enter a valid email address."
+              required
+              className="p-4 m-4 bg-slate-800 rounded-md text-white"
+            />
+            <input
+              type="text"
+              name="phoneNumber"
+              id="phoneNumber"
+              maxLength="10"
+              minLength="10"
+              pattern="^[0-9]{10}$"
+              title="Please enter a valid phone number"
+              required
+              value={contactFormData.phoneNumber}
+              onChange={handleOnChange}
+              placeholder="Phone Number"
+              className="p-4 m-4 bg-slate-800 rounded-md text-white"
+            />
+            <button
+              type="submit"
+              className="bg-green-700 m-4 pl-24 pr-24 pt-4 pb-4  rounded-md"
+            >
+              Send Now
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
