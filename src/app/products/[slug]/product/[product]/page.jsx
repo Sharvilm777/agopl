cd8e31c8230a9f8baf57117aff43050619367171
Sharvil <sharvilm777@gmail.com>
2023-08-11 14:52:17 +0530

    Trying to solve error

--- src/app/products/[slug]/product/[product]/page.jsx
+++ src/app/products/[slug]/product/[product]/page.jsx
@@ -1,53 +1,54 @@
 import ImageSlider from "@/components/ImageSlider";
 export default async function Page({ params }) {
-  const data = await fetch(
-    `http:localhost:3000/api/products?slug=${params.product}`
-  );
-  const res = await data.json();
-  console.log(res);
-  return (
-    <div>
-      <div className="container  flex flex-row">
-        <div className="productSlider w-[45%] min-h-screen flex justify-center items-center ml-8">
-          <ImageSlider images={res.images} />
-        </div>
+  return <div>product {params.product}</div>;
+  // const data = await fetch(
+  //   `http://localhost:3000/api/products?slug=${params.product}`
+  // );
+  // const res = await data.json();
+  // console.log(res);
+  // return (
+  //   <div>
+  //     <div className="container  flex flex-row">
+  //       <div className="productSlider w-[45%] min-h-screen flex justify-center items-center ml-8">
+  //         <ImageSlider images={res.images} />
+  //       </div>
 
-        <div className="productDescription w-[55%] min-h-screen m-6 flex flex-col justify-center items-center">
-          <h1 className="font-bold text-2xl text-center">
-            {res.slug.toUpperCase()}
-          </h1>
-          <h2 className="text-lg mb-2">
-            Pheromones lure for{" "}
-            <span className="font-semibold">{res.pest_name}</span>
-          </h2>
-          <h2 className="p-3">{res.description}</h2>
-          <h2 className="font-bold">{res.tip}</h2>
-          <div className="detailsContainer m-4 flex justify-center p-6 bg-slate-100">
-            <div class="grid grid-cols-2 gap-4">
-              <div class="text-lg font-semibold">
-                <div class="mb-2">Field viability:</div>
-                <div class="mb-2">Trap used :</div>
-                <div class="mb-2">No. of traps per acre:</div>
-                <div class="mb-2">Shelf life:</div>
-                <div class="mb-2">​Target crop:</div>
-              </div>
+  //       <div className="productDescription w-[55%] min-h-screen m-6 flex flex-col justify-center items-center">
+  //         <h1 className="font-bold text-2xl text-center">
+  //           {res.slug.toUpperCase()}
+  //         </h1>
+  //         <h2 className="text-lg mb-2">
+  //           Pheromones lure for{" "}
+  //           <span className="font-semibold">{res.pest_name}</span>
+  //         </h2>
+  //         <h2 className="p-3">{res.description}</h2>
+  //         <h2 className="font-bold">{res.tip}</h2>
+  //         <div className="detailsContainer m-4 flex justify-center p-6 bg-slate-100">
+  //           <div class="grid grid-cols-2 gap-4">
+  //             <div class="text-lg font-semibold">
+  //               <div class="mb-2">Field viability:</div>
+  //               <div class="mb-2">Trap used :</div>
+  //               <div class="mb-2">No. of traps per acre:</div>
+  //               <div class="mb-2">Shelf life:</div>
+  //               <div class="mb-2">​Target crop:</div>
+  //             </div>
 
-              <div className="">
-                <div className="mb-2">{res.field_viability}</div>
-                <div className="mb-2">{res.recommended_trap}</div>
-                <div className="mb-2">{res.trap_density}</div>
-                <div className="mb-2"></div>
-                <div className="mb-2"></div>
-                <div className="mb-2"> 1 year from the date of manufacture</div>
-                <div className="mb-2">{res.target_crop}</div>
-              </div>
-            </div>
-          </div>
-          <button className="text-green-500 border border-green-400 rounded-md p-3 pl-5 pr-5">
-            Get Quote
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+  //             <div className="">
+  //               <div className="mb-2">{res.field_viability}</div>
+  //               <div className="mb-2">{res.recommended_trap}</div>
+  //               <div className="mb-2">{res.trap_density}</div>
+  //               <div className="mb-2"></div>
+  //               <div className="mb-2"></div>
+  //               <div className="mb-2"> 1 year from the date of manufacture</div>
+  //               <div className="mb-2">{res.target_crop}</div>
+  //             </div>
+  //           </div>
+  //         </div>
+  //         <button className="text-green-500 border border-green-400 rounded-md p-3 pl-5 pr-5">
+  //           Get Quote
+  //         </button>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 }
